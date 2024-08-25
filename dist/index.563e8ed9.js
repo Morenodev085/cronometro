caefddbbe328ac41215c99c6c614d130d9257a51
Morenodev085 <moreno.ferreira11@gmail.com>
2024-08-25 02:19:52 -0300

    Add files via upload

---
+++ dist/index.563e8ed9.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Feb 25, 2024 22:00:00").getTime();setInterval(function(){let t=e-new Date().getTime(),n=Math.floor(t/864e5),o=Math.floor(t%864e5/36e5),a=Math.floor(t%6e4/1e3);document.getElementById("contador").innerHTML=`${n}d ${o}h ${a}s`},1e3);
+//# sourceMappingURL=index.563e8ed9.js.map
